d9b34d7b3e5151dc8028a69744d94c76d73b0a36
Ben Forster <ben.18.forster@gmail.com>
2021-07-24 20:57:20 +0100

    changed files

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require('express');
 const app = express();
 const socketio = require('socket.io');
 const mongoose = require('mongoose');
+const path = require('path');
 
-const server = app.listen(3001, () => console.log('listening'));
+const port = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirame, 'client/build', 'index.html'));
+    });
+}
+const server = app.listen(port, () => console.log('listening'));
 const io = socketio(server, {
     cors: {
-        origin:'http://localhost:3000'
+        origin:'*'
     }
 }); 
 
