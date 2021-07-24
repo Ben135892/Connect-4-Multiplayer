0372409e749790d97200203caa2cbdcfbd5fc6eb
Ben Forster <ben.18.forster@gmail.com>
2021-07-24 21:01:28 +0100

    fixed typo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirame, 'client/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
     });
 }
 const server = app.listen(port, () => console.log('listening'));
