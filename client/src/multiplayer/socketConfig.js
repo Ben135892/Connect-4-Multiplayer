d9edcadcafa57ab9406895724c4c4860afb99fd6
Ben Forster <ben.18.forster@gmail.com>
2021-07-25 00:40:44 +0100

    changed link styles

--- client/src/multiplayer/socketConfig.js
+++ client/src/multiplayer/socketConfig.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
-//const socket = io('/'); for deploymnet, io('http://localhost:3001') for local 
+//const socket = io('/'); for deploymnet, io('http://localhost:3001') for local testing
 const socket = io('/');
 export default socket;
