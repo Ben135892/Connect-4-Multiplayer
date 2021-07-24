bd9af8a5d0dc8cd97754f49ec51855cbfb9c2905
Ben Forster <ben.18.forster@gmail.com>
2021-07-24 21:47:06 +0100

    updates gitignore

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 const port = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });
