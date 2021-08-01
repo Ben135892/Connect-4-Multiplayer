5f6b2660c5a2f8de4c4faee4f45923d802e064c2
Ben Forster <ben.18.forster@gmail.com>
2021-08-01 21:01:57 +0100

    Removed unnecessary react files

--- client/src/components/multiplayer/Board.js
+++ client/src/components/multiplayer/Board.js
@@ -24,7 +24,7 @@ const Board = ({ game, setGame, playerColour, setGameOutcome }) => {
         socket.on('update-board', ({ row, col, colour }) => {
             setBoard(board => {
                 board[col][row] = colour;
-                return board;
+                return [...board];
             });
         });
         return () => {
